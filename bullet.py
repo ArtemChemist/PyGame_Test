f0938f388b683463bf0f28161dee324576f8ef20
Artem <lebedevfedora@gmail.com>
2021-03-26 23:23:23 +0300

    bullet methods added

--- bullet.py
+++ bullet.py
@@ -3,7 +3,7 @@ from pygame.sprite import Sprite
 
 class Bullet(Sprite):
     def __init__(self, ai_settings, screen, ship):
-        super(Bulet, self).__init__()
+        super().__init__()
         self.set = ai_settings
         self.screen = screen
 
@@ -16,3 +16,9 @@ class Bullet(Sprite):
         self.speed = ai_settings.bullet_speed
        
         self.moving = False
+    def update():
+        """ moves bullet image once it's fired"""
+        self.y -=self.speed
+        self.rect.y = init(self.y)
+    def draw (self):
+        pygem.draw.rect(self.screen, self.color, self.rect)
